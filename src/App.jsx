927cf78aef00162fd4b8211775f281b335ad7f0c
ggssDEVESH <deveshgokavarapu8@gmail.com>
2025-01-28 19:36:03 +0530

    main - basic setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <h1 class="text-2xl font-bold underline">Hello GOD!</h1>;
+}
+
+export default App;
