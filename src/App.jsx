7aeed891102713380054047ff5967763e09002dd
ggssDEVESH <deveshgokavarapu8@gmail.com>
2025-01-30 23:07:05 +0530

    MAIN assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Highlights from "./components/Highlights";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1 class="text-2xl font-bold underline">Hello GOD!</h1>;
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  );
 }
 
 export default App;
